245d5d3c610e9b80b95ec607ad8215453cbcb264
Andrey <losslessring@yandex.ru>
2023-04-20 02:28:53 +0300

    Draw cube, functional camera animation

--- src/main.js
+++ src/main.js
@@ -3,11 +3,12 @@ import { toMesh } from "./js/functions/toMesh/toMesh.js"
 import { toPoint } from "./js/functions/toPoint/toPoint.js"
 import { toPolygon } from "./js/functions/toPolygon/toPolygon.js"
 import { square } from "./js/models/square.js"
-import { doubleSquare } from './js/models/doubleSquare.js'
+import { doubleSquare } from "./js/models/doubleSquare.js"
+import { cube } from "./js/models/cubeObj.js"
 import { perspective } from "./js/functions/camera/perspective/perspective.js"
 import { offsetToCenter } from "./js/functions/utils/offsetToCenter/offsetToCenter.js"
 import { zoom } from "./js/functions/camera/zoom/zoom.js"
-import {camera} from './js/functions/camera/camera/camera.js'
+import { camera } from "./js/functions/camera/camera/camera.js"
 
 const canvas = document.querySelector("canvas")
 
@@ -16,16 +17,33 @@ const context = canvas.getContext("2d")
 //console.log(square.map(shape=>shape.map(toPoint)))
 // console.log(square.map(toPolygon))
 
-const mesh = toMesh(doubleSquare)
+const mesh = toMesh(cube)
 
 //const cam = camera(100, 8)
 context.strokeStyle = "#fff"
-mesh.forEach((polygon) => {
-    drawPolygon(context)(
-        polygon
-            //.map((point) => perspective(point, 100))
-            //.map((point) => zoom(point, 8))
-            .map(point => camera(100, 8)(point))
-            .map(point => offsetToCenter(point, context.canvas))
-    )
-})
+
+//const perspCamera = camera(200, 12)
+
+const drawMesh = cameraWithDistanceAndZoom => mesh => {
+    mesh.forEach((polygon) => {
+        drawPolygon(context)(
+            polygon
+                //.map((point) => perspective(point, 100))
+                //.map((point) => zoom(point, 8))
+                //.map((point) => camera(200, 12)(point))
+                .map((point) => cameraWithDistanceAndZoom(point))
+                .map((point) => offsetToCenter(point, context.canvas))
+        )
+    })
+}
+
+//drawMesh(perspCamera)(mesh)
+
+const animate = (accumulator, increment) => () => {
+    context.clearRect(0, 0, canvas.width, canvas.height)
+    const perspCamera = camera(accumulator, 12)
+    drawMesh(perspCamera)(mesh)
+    requestAnimationFrame(animate(accumulator + increment, increment))
+}
+
+animate(200, 0.1)()
